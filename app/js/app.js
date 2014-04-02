28fcc43c991910b77831c496accbc358b7e60942
amy <amy.mccarthy2@mail.dcu.ie>
2014-04-02 21:01:32 +0100

    made layout responsive

--- app/js/app.js
+++ app/js/app.js
@@ -6,20 +6,13 @@ var bestsellersApp = angular.module('bestsellersApp', [
   'ngRoute',
   'bestsellersControllers',
 ]);
-  
-bestsellersApp.config(['$routeProvider',
+ 
+bestsellersApp.config(['$routeProvider',  
   function($routeProvider) {
     $routeProvider.
       when('/bestsellers', {
         templateUrl: 'partials/bestseller-list.html',
         controller: 'BestsellersCtrl'
-      }).
-       when('/bestsellers/bestsellers/:list_image', {
-        templateUrl: 'partials/bestseller-detail.html',
-        controller: 'BestsellerDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/bestsellers'
       });
   }]);
 
